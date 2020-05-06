49f42e6b8f69b0bb24a643a5a321a99c365d2c72
328730316@qq.com <328730316@qq.com>
2020-05-06 16:50:54 +0800

    create a README.md file and rewrite the translate.py file

---
+++ README.md
@@ -0,0 +1,38 @@
+# RLComGen
+
+RLCodeGen is a project using reinforcement learning algorithm Actor-Critic to generate summarization for code snippet.
+The source code and dataset are opened.
+
+# Requirments
+
+Python 3.7
+
+Pytorch 1.4
+
+# Config
+
+Every model has its own `config.py` file in the model folder, we can change the configuration in this file.
+
+# Model Training
+
+Pretrain the actor of RLCom: `python main.py --method RLCom --pretrain actor`
+
+Pretrain the critic of RLCom: `python main.py --method RLCom --pretrain critic`
+
+Train the RLCom: `python main.py --method RLCom`
+
+Train the deepcom: `python main.py --method deepcom`
+
+Train the TL-CodeSum: `python main.py --method TLCodeSum`
+
+RLComAPI is the model RLCom with transferred api knowledge, that's to say, is using TL-CodeSum as actor and critic in Actor-Critic algorithm. It is the same usage with RLCom and RLComAPI.
+
+In every training epoch, if the model has the greater performance, this model will be saved in "[model_name]/save/ast_[training_epoch].pkl".
+
+Continue to train the model starting from a checkpoint: `python main.py --method [model_name] --start [training_epoch]`
+
+# Translation
+
+`python translate.py --method [model_name] --start [training_epoch]`
+
+This instruction translates the test code snippets into their summarizations. The result is saved in "pred.txt".
--- translate.py
+++ translate.py
@@ -9,40 +9,36 @@ import torch
 from prefetch_generator import BackgroundGenerator
 
 method = ""
-pretrain = ""
+datatype = "ast"
 start = -1
-options, args = getopt.getopt(sys.argv[1:],"",['method=','start=','pretrain='])
+options, args = getopt.getopt(sys.argv[1:],"",['method=','start=','datatype='])
 for name, value in options:
     if name == "--method":
         method = value
     elif name == "--start":
         start = int(value)
-    elif name == "--pretrain":
-        pretrain = value
+    elif name == "--datatype":
+        datatype = value
 
 dict_path = "./dataset/"
-ast_w2i = pickle.load(open(os.path.join(dict_path,"ast_w2i.pkl"),"rb"))
-ast_i2w = pickle.load(open(os.path.join(dict_path,"ast_i2w.pkl"),"rb"))
+if datatype == "ast":
+    ast_w2i = pickle.load(open(os.path.join(dict_path,"ast_w2i.pkl"),"rb"))
+    ast_i2w = pickle.load(open(os.path.join(dict_path,"ast_i2w.pkl"),"rb"))
+else:
+    ast_w2i = pickle.load(open(os.path.join(dict_path,"code_w2i.pkl"),"rb"))
+    ast_i2w = pickle.load(open(os.path.join(dict_path,"code_i2w.pkl"),"rb"))
 nl_w2i = pickle.load(open(os.path.join(dict_path,"nl_w2i.pkl"),"rb"))
 nl_i2w = pickle.load(open(os.path.join(dict_path,"nl_i2w.pkl"),"rb"))
 api_w2i = pickle.load(open(os.path.join(dict_path,"api_w2i.pkl"),"rb"))
 api_i2w = pickle.load(open(os.path.join(dict_path,"api_i2w.pkl"),"rb"))
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-log_filename = "./{}/log".format(method)
-fh = logging.FileHandler(log_filename)
-ch = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-logger.addHandler(fh)
-logger.addHandler(ch)
-
 dataset_path = "./dataset/"
-valid_ast = os.path.join(dataset_path,"valid.token-nl2.token")
-valid_nl = os.path.join(dataset_path,"valid.token-nl2.nl2")
-valid_api = os.path.join(dataset_path,"valid.token-nl2.api")
+if datatype == "ast":
+    test_ast = os.path.join(dataset_path,"test.token-nl2.token")
+else:
+    test_ast = os.path.join(dataset_path,"test.token-nl2.code2")
+test_nl = os.path.join(dataset_path,"test.token-nl2.nl2")
+test_api = os.path.join(dataset_path,"test.token-nl2.api")
 
 if method == "deepcom":
     from deepcom.deepcom import *
@@ -66,14 +62,14 @@ elif method == "RLComAPI":
     model = RLComAPI(config)
 
 if start != -1:
-    model.load("./{}/save/{}.pkl".format(method,start))
+    model.load("./{}/save/{}_{}.pkl".format(method,datatype,start))
     model.set_trainer()
         
 has_valid = 0
-valid_dataset = BackgroundGenerator(get_batch(valid_ast,valid_nl,config.EVAL_BATCH_SIZE,valid_api), 1)
+test_dataset = BackgroundGenerator(get_batch(test_ast,test_nl,config.TEST_BATCH_SIZE,test_api), 1)
 corpus = []
-for x_raw,y_raw,z_raw in valid_dataset:
-    has_valid += 1
+for x_raw,y_raw,z_raw in test_dataset:
+    has_test += 1
     x = [[ast_w2i[token] if token in ast_w2i else ast_w2i['<UNK>'] for token in line[:config.MAX_SEQ_LEN]] for line in x_raw]
     if method == "TLCodeSum" or method == "RLComAPI":
         z = [[api_w2i[token] if token in api_w2i else api_w2i['<UNK>'] for token in line[:config.MAX_API_LEN]] for line in z_raw]   
